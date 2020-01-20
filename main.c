d57ae55c2de6b69783c0c98c2b5269a239dbf8a0
Darren Ng <darren19970810@gmail.com>
2020-01-21 00:29:42 +0800

    4*4 matrix ok on circleci

--- main.c
+++ main.c
@@ -2,17 +2,20 @@
 
 // /home/darren/pdf/Computer-Organization-and-Design-The-Hardware-Software-Interface-5th-Edition-With-all-appendices-and-advanced-material.pdf
 
-// gcc -std=c11 -g -O0 -Wall -Wextra -Wno-unused-parameter -fmax-errors=1 matrix.c
-// gcc -std=c11 -g -O0 -Wall -Wextra -Wno-unused-parameter -mavx -S matrix.c
 // gcc -std=c11 -g -O0 -Wall -Wextra -Wno-unused-parameter -mavx main.c
 
 // /usr/lib/gcc/x86_64-pc-linux-gnu/9.2.0/include/x86intrin.h
 #include <x86intrin.h>
 #include <stdio.h>
 
+int n=0;
+double *A=NULL;
+double *B=NULL;
+double *C=NULL;
+
 // square n*n matrix
 // n=0,4,8,...
-void dgemm(int n,double *A,double *B,double *C){
+void dgemm(){
   for(int i=0;i<n;i+=4){
     for(int j=0;j<n;j++){
       __m256d c0=_mm256_load_pd(C+i+j*n);/* c0=C[i][j] */
@@ -31,35 +34,56 @@ void dgemm(int n,double *A,double *B,double *C){
   }
 }
 
-void print(int n,double *m){
+void print(const double *m){
   for(int r=0;r<n;++r){
     for(int c=0;c<n;++c)
-      printf("%f ",m[r][c]);
+      printf("%f ",((double (*)[n])m)[r][c]);
     printf("\n");
   }
 }
 
-void init()
+void init(){
+  n=4;
+  A=calloc(n*n,sizeof(double));
+  B=calloc(n*n,sizeof(double));
+  C=calloc(n*n,sizeof(double));
+
+  int val=0;
+
+  // (int[3]){0,1,2}[2]++;
+  // (double*[3]){NULL,NULL,NULL};
+
+  for(int i=0;i<2;++i){
+    for(int r=0;r<n;++r){
+      for(int c=0;c<n;++c){
+        ((double (*)[n])((double*[2]){A,B}[i]))[r][c]=val;
+        ++val;
+      }
+    }
+  }
+}
+
+void end(){
+  free(A);A=NULL;
+  free(B);B=NULL;
+  free(C);C=NULL;
+}
 
 int main(){
 
-  // double a[1][1]={{12.34}};
-  // double b[1][1]={{56.78}};
-  // double c[1][1]={{}};
+  init();
+  print(A);printf("\n");
+  print(B);printf("\n");
+  print(C);printf("\n");
+  printf("\n");
 
-  // double a[1]={12.34};
-  // double b[1]={56.78};
-  // double c[1]={};
+  dgemm();
+  print(A);printf("\n");
+  print(B);printf("\n");
+  print(C);printf("\n");
+  printf("\n");
 
-// Text Pastry: range 0 32 1
+  end();
 
-  // double a[4][4]={12.34};
-  // double b[4][4]={56.78};
-  // double c[4][4]={{}};
-  // dgemm(4,a,b,c);
-
-  dgemm(0,NULL,NULL,NULL);
-
-  printf("%f\n",c[0]);
   return 0;
 }
