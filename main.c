5481febe5248509617024b2a9c00d98b55a34f40
Darren Ng <darren19970810@gmail.com>
2020-01-21 00:04:20 +0800

    before switch to globals

--- main.c
+++ main.c
@@ -4,12 +4,14 @@
 
 // gcc -std=c11 -g -O0 -Wall -Wextra -Wno-unused-parameter -fmax-errors=1 matrix.c
 // gcc -std=c11 -g -O0 -Wall -Wextra -Wno-unused-parameter -mavx -S matrix.c
-// gcc -std=c11 -g -O0 -Wall -Wextra -Wno-unused-parameter -mavx matrix.c
+// gcc -std=c11 -g -O0 -Wall -Wextra -Wno-unused-parameter -mavx main.c
 
 // /usr/lib/gcc/x86_64-pc-linux-gnu/9.2.0/include/x86intrin.h
 #include <x86intrin.h>
 #include <stdio.h>
 
+// square n*n matrix
+// n=0,4,8,...
 void dgemm(int n,double *A,double *B,double *C){
   for(int i=0;i<n;i+=4){
     for(int j=0;j<n;j++){
@@ -29,14 +31,35 @@ void dgemm(int n,double *A,double *B,double *C){
   }
 }
 
+void print(int n,double *m){
+  for(int r=0;r<n;++r){
+    for(int c=0;c<n;++c)
+      printf("%f ",m[r][c]);
+    printf("\n");
+  }
+}
+
+void init()
+
 int main(){
+
   // double a[1][1]={{12.34}};
   // double b[1][1]={{56.78}};
   // double c[1][1]={{}};
-  double a[1]={12.34};
-  double b[1]={56.78};
-  double c[1]={};
-  dgemm(1,a,b,c);
+
+  // double a[1]={12.34};
+  // double b[1]={56.78};
+  // double c[1]={};
+
+// Text Pastry: range 0 32 1
+
+  // double a[4][4]={12.34};
+  // double b[4][4]={56.78};
+  // double c[4][4]={{}};
+  // dgemm(4,a,b,c);
+
+  dgemm(0,NULL,NULL,NULL);
+
   printf("%f\n",c[0]);
   return 0;
 }
