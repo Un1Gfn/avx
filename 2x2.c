3163fcb141d4693f6e344c96d406302eb43f5308
Darren Ng <darren19970810@gmail.com>
2020-02-13 12:59:08 +0800

    2x2.c finish

--- 2x2.c
+++ 2x2.c
@@ -14,12 +14,11 @@
 // Variables and data arrays used by plot generators
 static PLFLT        x[101], y[101];
 static PLFLT        xscale, yscale, xoff, yoff, xs[6], ys[6];
-static int          test_xor;
 
 PLCHAR_VECTOR        notes[] = { "Make sure you get it right!", NULL };
 
 // Function prototypes
-void plot1(int do_test);
+void plot1();
 void plot2();
 void plot3();
 
@@ -42,10 +41,12 @@ int main(){
   // Initialize plplot
   // Divide page into 2x2 plots
   // Note: calling plstar replaces separate calls to plssub and plinit
-  plsdev("qtwidget");
-  plsetopt("geometry","+100+100");
+  plsdev("xcairo");
+  // plsdev("xwin");
+  // plsdev("qtwidget");
+  // plsetopt("geometry","+100+100");
   plstar( 2, 2 );
-  plfontld( 1 );
+  // plfontld( 0 );
 
   // Set up the data
   // Original case
@@ -54,7 +55,7 @@ int main(){
   xoff   = 0.;
   yoff   = 0.;
   // Do a plot
-  plot1( 0 );
+  plot1();
 
   // Set up the data
   xscale = 1.;
@@ -63,7 +64,7 @@ int main(){
   // Do a plot
   digmax = 5;
   plsyax( digmax, 0 );
-  plot1( 1 );
+  plot1();
   plot2();
   plot3();
 
@@ -75,152 +76,110 @@ int main(){
 //--------------------------------------------------------------------------
 
 void
-plot1( int do_test )
-{
-    int   i;
-    PLFLT xmin, xmax, ymin, ymax;
+plot1(){
+  int   i;
+  PLFLT xmin, xmax, ymin, ymax;
 
-    for ( i = 0; i < 60; i++ )
-    {
-        x[i] = xoff + xscale * ( i + 1 ) / 60.0;
-        y[i] = yoff + yscale * pow( x[i], 2. );
-    }
+  for ( i = 0; i < 60; i++ ){
+    x[i] = xoff + xscale * ( i + 1 ) / 60.0;
+    y[i] = yoff + yscale * pow( x[i], 2. );
+  }
 
-    xmin = x[0];
-    xmax = x[59];
-    ymin = y[0];
-    ymax = y[59];
+  xmin = x[0];
+  xmax = x[59];
+  ymin = y[0];
+  ymax = y[59];
 
-    for ( i = 0; i < 6; i++ )
-    {
-        xs[i] = x[i * 10 + 3];
-        ys[i] = y[i * 10 + 3];
-    }
+  for ( i = 0; i < 6; i++ ){
+      xs[i] = x[i * 10 + 3];
+      ys[i] = y[i * 10 + 3];
+  }
 
-// Set up the viewport and window using PLENV. The range in X is
-// 0.0 to 6.0, and the range in Y is 0.0 to 30.0. The axes are
-// scaled separately (just = 0), and we just draw a labelled
-// box (axis = 0).
-//
-    plcol0( 1 );
-    plenv( xmin, xmax, ymin, ymax, 0, 0 );
-    plcol0( 2 );
-    pllab( "(x)", "(y)", "#frPLplot Example 1 - y=x#u2" );
+  // Set up the viewport and window using PLENV. The range in X is
+  // 0.0 to 6.0, and the range in Y is 0.0 to 30.0. The axes are
+  // scaled separately (just = 0), and we just draw a labelled
+  // box (axis = 0).
+  plcol0( 1 );
+  plenv( xmin, xmax, ymin, ymax, 0, 0 );
+  plcol0( 2 );
+  pllab( "(x)", "(y)", "#frPLplot Example 1 - y=x#u2" );
 
-// Plot the data points
+  // Plot the data points
+  plcol0( 4 );
+  plpoin( 6, xs, ys, 9 );
 
-    plcol0( 4 );
-    plpoin( 6, xs, ys, 9 );
+  // Draw the line through the data
+  plcol0( 3 );
+  plline( 60, x, y );
 
-// Draw the line through the data
-
-    plcol0( 3 );
-    plline( 60, x, y );
-
-// xor mode enable erasing a line/point/text by replotting it again
-// it does not work in double buffering mode, however
-
-    if ( do_test && test_xor )
-    {
-#ifdef PL_HAVE_NANOSLEEP
-        PLINT           st;
-        struct timespec ts;
-        ts.tv_sec  = 0;
-        ts.tv_nsec = 50000000;
-        plxormod( 1, &st ); // enter xor mode
-        if ( st )
-        {
-            for ( i = 0; i < 60; i++ )
-            {
-                plpoin( 1, x + i, y + i, 9 );   // draw a point
-                nanosleep( &ts, NULL );         // wait a little
-                plflush();                      // force an update of the tk driver
-                plpoin( 1, x + i, y + i, 9 );   // erase point
-            }
-            plxormod( 0, &st );                 // leave xor mode
-        }
-#else
-        printf( "The -xor command line option can only be exercised if your "
-            "system\nhas nanosleep(), which does not seem to happen.\n" );
-#endif
-    }
 }
 
 //--------------------------------------------------------------------------
 
-void
-plot2( void )
-{
-    int i;
+void plot2(){
+  int i;
 
-// Set up the viewport and window using PLENV. The range in X is -2.0 to
-// 10.0, and the range in Y is -0.4 to 2.0. The axes are scaled separately
-// (just = 0), and we draw a box with axes (axis = 1).
-//
-    plcol0( 1 );
-    plenv( -2.0, 10.0, -0.4, 1.2, 0, 1 );
-    plcol0( 2 );
-    pllab( "(x)", "sin(x)/x", "#frPLplot Example 1 - Sinc Function" );
+  // Set up the viewport and window using PLENV. The range in X is -2.0 to
+  // 10.0, and the range in Y is -0.4 to 2.0. The axes are scaled separately
+  // (just = 0), and we draw a box with axes (axis = 1).
+  //
+  plcol0( 1 );
+  plenv( -2.0, 10.0, -0.4, 1.2, 0, 1 );
+  plcol0( 2 );
+  pllab( "(x)", "sin(x)/x", "#frPLplot Example 1 - Sinc Function" );
 
-// Fill up the arrays
+  // Fill up the arrays
 
-    for ( i = 0; i < 100; i++ )
-    {
-        x[i] = ( i - 19.0 ) / 6.0;
-        y[i] = 1.0;
-        if ( x[i] != 0.0 )
-            y[i] = sin( x[i] ) / x[i];
-    }
+  for ( i = 0; i < 100; i++ ){
+    x[i] = ( i - 19.0 ) / 6.0;
+    y[i] = 1.0;
+    if ( x[i] != 0.0 )
+      y[i] = sin( x[i] ) / x[i];
+  }
 
-// Draw the line
+  // Draw the line
 
-    plcol0( 3 );
-    plwidth( 2 );
-    plline( 100, x, y );
-    plwidth( 1 );
+  plcol0( 3 );
+  plwidth( 2 );
+  plline( 100, x, y );
+  plwidth( 1 );
 }
 
 //--------------------------------------------------------------------------
 
-void
-plot3( void )
-{
-    PLINT space0 = 0, mark0 = 0, space1 = 1500, mark1 = 1500;
-    int   i;
+void plot3(){
+  PLINT space0 = 0, mark0 = 0, space1 = 1500, mark1 = 1500;
+  int   i;
 
-// For the final graph we wish to override the default tick intervals, and
-// so do not use plenv().
-//
-    pladv( 0 );
+  // For the final graph we wish to override the default tick intervals, and
+  // so do not use plenv().
+  pladv( 0 );
 
-// Use standard viewport, and define X range from 0 to 360 degrees, Y range
-// from -1.2 to 1.2.
-//
-    plvsta();
-    plwind( 0.0, 360.0, -1.2, 1.2 );
+  // Use standard viewport, and define X range from 0 to 360 degrees, Y range
+  // from -1.2 to 1.2.
+  plvsta();
+  plwind( 0.0, 360.0, -1.2, 1.2 );
 
-// Draw a box with ticks spaced 60 degrees apart in X, and 0.2 in Y.
+  // Draw a box with ticks spaced 60 degrees apart in X, and 0.2 in Y.
+  plcol0( 1 );
+  plbox( "bcnst", 60.0, 2, "bcnstv", 0.2, 2 );
 
-    plcol0( 1 );
-    plbox( "bcnst", 60.0, 2, "bcnstv", 0.2, 2 );
+  // Superimpose a dashed line grid, with 1.5 mm marks and spaces.
+  // plstyl expects a pointer!
+  plstyl( 1, &mark1, &space1 );
+  plcol0( 2 );
+  plbox( "g", 30.0, 0, "g", 0.2, 0 );
+  plstyl( 0, &mark0, &space0 );
 
-// Superimpose a dashed line grid, with 1.5 mm marks and spaces.
-// plstyl expects a pointer!
-//
-    plstyl( 1, &mark1, &space1 );
-    plcol0( 2 );
-    plbox( "g", 30.0, 0, "g", 0.2, 0 );
-    plstyl( 0, &mark0, &space0 );
+  plcol0( 3 );
+  pllab( "Angle (degrees)", "sine", "#frPLplot Example 1 - Sine function" );
 
-    plcol0( 3 );
-    pllab( "Angle (degrees)", "sine", "#frPLplot Example 1 - Sine function" );
+  for ( i = 0; i < 101; i++ )
+  {
+    x[i] = 3.6 * i;
+    y[i] = sin( x[i] * M_PI / 180.0 );
+  }
 
-    for ( i = 0; i < 101; i++ )
-    {
-        x[i] = 3.6 * i;
-        y[i] = sin( x[i] * M_PI / 180.0 );
-    }
-
-    plcol0( 4 );
-    plline( 101, x, y );
+  plcol0( 4 );
+  plline( 101, x, y );
 }
